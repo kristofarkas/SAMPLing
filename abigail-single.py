c8f9f1c4e1e09edc19a5179ebf73e6e9327f9fcd
Kristof Farkas-Pall <kristof.farkas-pall.14@ucl.ac.uk>
2018-05-08 10:24:20 +0100

    Add alchemical tags

--- abigail-single.py
+++ abigail-single.py
@@ -14,31 +14,33 @@ for s in system_names:
         top = AbFile(folder+"complex.prmtop", tag='topology').with_prefix(name)
         coord = AbFile(folder+"complex.rst7", tag='coordinate').with_prefix(name)
         rest = AbFile(rest_folder+"restraint_1.0.in", tag='restraint', needs_copying=True).with_prefix(name)
-        systems.append(System(name, files=[pdb, top, coord, rest]))
+        tags = AbFile(folder+"tags.pdb", tag='alchemicaltags').with_prefix(name).with_prefix(name)
+        systems.append(System(name, files=[pdb, top, coord, rest, tags]))
 
-esm = Protocol(clone_settings=False)
+# afe = Protocol(clone_settings=False)
+step = Afe.step0
+numsteps = Afe.numsteps[0]
 
-for step, numsteps in zip(Afe.steps, Afe.numsteps):
-    sim = Simulation()
-    sim.engine = 'namd_mpi'
-    sim.cores = 32
+sim = Simulation()
+sim.engine = 'namd_mpi'
+sim.cores = 32
 
-    sim.cutoff = 12.0
-    sim.pairlistdist = 13.5
-    sim.switchingdist = 10.0
-    sim.numsteps = step
+sim.cutoff = 12.0
+sim.pairlistdist = 13.5
+sim.switchingdist = 10.0
+sim.numsteps = step
 
-    sim.add_ensemble('replica', range(5))
-    sim.add_ensemble('system', systems)
-    sim.add_ensemble('lamdawindow', [0.0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+sim.add_ensemble('replica', values=range(5))
+sim.add_ensemble('system', systems)
+sim.add_ensemble('lamdawindow', [0.0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
 
-    sim.add_input_file(step, is_executable_argument=True)
+sim.add_input_file(step, is_executable_argument=True)
 
-    sim.add_variable(name='k1', value=10, in_file='*restraint_1.0.in')
-    sim.add_variable(name='k2', value=500, in_file='*restraint_1.0.in')
+sim.add_variable(name='k1', value=10, in_file='*restraint_1.0.in')
+sim.add_variable(name='k2', value=500, in_file='*restraint_1.0.in')
 
-    esm.append(sim)
+# afe.append(sim)
 
 runner = Runner(resource='bw_aprun', comm_server=('two.radical-project.org', 33166))
-runner.add_protocol(esm)
+runner.add_protocol(sim)
 runner.run(walltime=1440, queue='high')
