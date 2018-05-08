e94ad7dbeacf2c2a8721fdd1c471c2f574f76252
Kristof Farkas-Pall <kristof.farkas-pall.14@ucl.ac.uk>
2018-05-08 11:28:19 +0100

    Add BW run script

---
+++ bw-pbs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export RADICAL_PILOT_PROFILE=True
+export RADICAL_ENMD_PROFILE=True
+export RADICAL_ENMD_PROFILING=1
+export RP_ENABLE_OLD_DEFINES=True
+
+export RADICAL_ENTK_VERBOSE='DEBUG'
+export RADICAL_SAGA_VERBOSE='DEBUG'
+export RADICAL_PILOT_VERBOSE='DEBUG'
+
+export WORKDIR="/u/sciteam/farkaspa"
+export PATH="$WORKDIR/miniconda2/bin:$PATH"
+export LD_LIBRARY_PATH="$WORKDIR/miniconda2:$LD_LIBRARY_PATH"
+
+python abigail-single.py
