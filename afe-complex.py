28cdeb4136060b352a54d9d0987c5d8803a7ebd0
Kristof Farkas-Pall <kristof.farkas-pall.14@ucl.ac.uk>
2018-05-11 22:00:55 +0100

    Update afe-complex.py

--- afe-complex.py
+++ afe-complex.py
@@ -7,18 +7,17 @@ systems = list()
 
 for s in system_names:
     for r in range(5):
+        name = "{}-{}".format(s, r)
         folder = "{}/{}-{}/build/".format(prefix, s, r)
         rest_folder = "{}/{}-{}/restraints/".format(prefix, s, r)
-        name = "{}-{}".format(s, r)
+        
         pdb = AbFile(folder+"complex.pdb", tag='pdb').with_prefix(name)
         top = AbFile(folder+"complex.prmtop", tag='topology').with_prefix(name)
         coord = AbFile(folder+"complex.rst7", tag='coordinate').with_prefix(name)
         rest = AbFile(rest_folder+"restraint_1.0.in", tag='restraint', needs_copying=True).with_prefix(name)
         tags = AbFile(folder+"tags.pdb", tag='alchemicaltags').with_prefix(name).with_prefix(name)
+        
         systems.append(System(name, files=[pdb, top, coord, rest, tags]))
-
-step = Afe.step0
-numsteps = Afe.numsteps[0]
 
 sim = Simulation()
 sim.engine = 'namd_mpi'
@@ -27,17 +26,17 @@ sim.cores = 32
 sim.cutoff = 12.0
 sim.pairlistdist = 13.5
 sim.switchdist = 10.0
-sim.numsteps = numsteps
+sim.numsteps = Afe.numsteps[0]
 
-sim.add_ensemble('replica', values=range(2))
-sim.add_ensemble('system', values=systems[:2])
-sim.add_ensemble('lambdawindow', values=[0.0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0][:2])
+sim.add_ensemble('replica', values=range(5))
+sim.add_ensemble('system', values=systems)
+sim.add_ensemble('lambdawindow', values=[0.0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
 
-sim.add_input_file(step, is_executable_argument=True)
+sim.add_input_file(Afe.step0, is_executable_argument=True)
 
 sim.add_variable(name='k1', value=10, in_file='*restraint_1.0.in')
 sim.add_variable(name='k2', value=500, in_file='*restraint_1.0.in')
 
-runner = Runner(resource='bw_aprun', comm_server=('two.radical-project.org', 33166))
+runner = Runner(resource='local', comm_server=('two.radical-project.org', 33166))
 runner.add_protocol(sim)
-runner.run(walltime=1440, queue='high', access_schema='local')
+runner.run(walltime=1440, access_schema='local')
