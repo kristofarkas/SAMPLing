dc6f861153b71cb719c98db2b4e0a0389476719d
Kristof Farkas-Pall <kristof.farkas-pall.14@ucl.ac.uk>
2018-05-13 17:54:22 +0100

    Update dummy-run.sh

--- dummy-run.sh
+++ dummy-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export RADICAL_PILOT_DBURL='mongodb://user:user@ds239009.mlab.com:39009/inspire-2'
+export RADICAL_PILOT_DBURL=
 export RADICAL_PILOT_PROFILE=True
 export RADICAL_ENMD_PROFILE=True
 export RADICAL_ENMD_PROFILING=1
@@ -12,4 +12,4 @@ export RADICAL_PILOT_VERBOSE='DEBUG'
 
 # Activate, export PATH, etc.
 
-mprof run --include-children afe-dummy.py &> rp.session.log
+python afe-dummy.py &> rp.session.log
